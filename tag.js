9c3bcb17ca538e63e89b6a49c1a382fe99ea6901
Jonas Schumacher <jonas.merlin.schumacher@mailbox.org>
2022-10-26 08:47:02 +0200

    listen for shortcut

--- tag.js
+++ tag.js
@@ -1,11 +1,27 @@
+let controlDown = false;
+
 document.onkeydown = function (e) {
+  // Do nothing if the event was already processed
   if (e.defaultPrevented) {
-    return; // Do nothing if the event was already processed
+    return;
   }
 
-  switch (e.key) {
-    case "ArrowDown":
-      console.log("test");
-      break;
+  if (e.key === "Control") {
+    controlDown = true;
+  } else if (e.key === "t") {
+    if (controlDown) {
+      console.log("taggart");
+    }
+  }
+};
+
+document.onkeyup = function (e) {
+  // Do nothing if the event was already processed
+  if (e.defaultPrevented) {
+    return;
+  }
+
+  if (e.key === "Control") {
+    controlDown = false;
   }
 };
